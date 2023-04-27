ba8b4de509cd03c0e68dc69cd7df1e4a103782e8
lifthus <lifthus531@gmail.com>
2023-04-28 00:25:19 +0900

    😢 js modification for new name envbyjson done

--- examples/javascript/index.js
+++ examples/javascript/index.js
@@ -1,28 +1,28 @@
-import envjson from "envjson";
+import envbyjson from "envbyjson";
 
 // it loads env.json file in the root directory
-envjson.load()
+envbyjson.load()
 console.log(process.env.A)
 
 // it loads env2.json file in the testdata directory
-envjson.load("testdata/env2.json")
+envbyjson.load("testdata/env2.json")
 console.log(process.env.B)
 
 // it loads env3.json and env4.json file in the testdata directory
-envjson.loadProp("testdata/env3.json", "testdata/env4.json", "C") 
+envbyjson.loadProp("testdata/env3.json", "testdata/env4.json", "C") 
 console.log(process.env.D)
 console.log(process.env.E)
 
 try { 
     // it throws an error because one of the prop is not string
-    envjson.load("testdata/env_err.json")
+    envbyjson.load("testdata/env_err.json")
 } catch(e) {
     console.log(e.message)
 }
 
 try {
     // it throws an error because one of the prop is not string
-    envjson.loadProp("testdata/env_err2.json", "C")
+    envbyjson.loadProp("testdata/env_err2.json", "C")
 } catch(e) {
     console.log(e.message)
 }
