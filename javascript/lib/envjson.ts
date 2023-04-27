154cbbf90eb69722aaab1ebffac9a5d166fddd8c
lifthus <lifthus531@gmail.com>
2023-04-27 18:14:12 +0900

    js envjson done in ts

---
+++ javascript/lib/envjson.ts
@@ -0,0 +1,76 @@
+/**
+ * loads given json files and sets the properties to the environment variables.
+ * if no path is given, it loads env.json file in the current directory.
+ *
+ * note that it doesn't override existing environment variables.
+ *
+ * example:
+ * envjson.Load() // it loads env.json file by default
+ * envjson.Load("env2.json", "env3.json")
+ *
+ * @param paths - json file paths
+ */
+export const load = (...paths: string[]) => {
+  for (const path of paths) {
+    // if wrong path is given, it throws an error
+    const envVars = require(path);
+    for (const k in envVars) {
+      if (typeof envVars[k] !== "string") {
+        throw Error(`property ${k} is not string`);
+      }
+      if (process.env[k] === undefined) {
+        process.env[k] = envVars[k];
+      }
+    }
+  }
+};
+
+/**
+ * loads specific property from given json files and sets the environment variables.
+ * at least one param which specifies the property must be given.
+ *
+ * there are two ways to specify the property.
+ * 1. when only one param is given, it loads given property from env.json file in the current directory.
+ * 2. when more than one params are given, it loads property named same as the last param from the json files specified by preceding params.
+ *
+ * note that it doesn't override existing environment variables.
+ *
+ * example:
+ *
+ * envjson.LoadProp("prop1") // it loads "prop1" property from env.json file by default
+ * envjson.LoadProp("env2.json", "env3.json", "prop1")
+ *
+ *  @param params - property name and json file paths
+ */
+export const loadProp = (...params: string[]) => {
+  if (params.length === 0) {
+    console.error("property name must be given");
+    throw Error("property name must be given");
+  }
+  if (params.length === 1) {
+    params = ["env.json", params[0]];
+  }
+
+  const propName = params[params.length - 1];
+
+  for (const path of params.slice(0, params.length - 1)) {
+    // if wrong path is given, it throws an error
+    const envVars = require(path);
+    // from specified property
+    for (const k in envVars[propName]) {
+      if (typeof envVars[k] !== "string") {
+        throw Error(`property ${k} is not string`);
+      }
+      if (process.env[k] === undefined) {
+        process.env[k] = envVars[k];
+      }
+    }
+  }
+};
+
+const envjsonModule = {
+  load,
+  loadProp,
+};
+
+export default envjsonModule;
